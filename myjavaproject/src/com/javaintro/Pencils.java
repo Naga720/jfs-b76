f05405146478a0113c1311fc1afbbe6f0699fd66
kodar <kodar@Nagaraju>
2026-07-28 09:35:18 +0530

    my second comit

---
+++ myjavaproject/src/com/javaintro/Pencils.java
@@ -0,0 +1,14 @@
+package com.javaintro;
+
+public class Pencils {
+
+	public static void main(String[] args) {
+		int cost = 7;
+		int amount = 100;
+		int pencils = amount/cost;
+		System.out.println("Total pencils:="+pencils);
+		System.out.println("Remaining amount is:"+amount%cost);
+
+	}
+
+}
